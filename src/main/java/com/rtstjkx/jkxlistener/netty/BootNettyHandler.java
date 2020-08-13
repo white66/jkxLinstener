6bf7a007b5f2dcb3a450643d3893ad5ab9c7fd76
white66 <45169774+white66@users.noreply.github.com>
2020-08-13 11:11:51 +0800

    更改bootnettyhandler类中导入包，properties中端口号，优化日志文件中的log文件路径

--- src/main/java/com/rtstjkx/jkxlistener/netty/BootNettyHandler.java
+++ src/main/java/com/rtstjkx/jkxlistener/netty/BootNettyHandler.java
@@ -21,7 +21,6 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
